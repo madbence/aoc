029eab4ee9d22096a286adcc7a5365a0074bfb1d
Bence Dányi <bence@danyi.me>
2024-12-04 19:10:52 +0100

    simplify transpose

--- clojure/src/aoc/core.clj
+++ clojure/src/aoc/core.clj
@@ -16,9 +16,4 @@
 (defn transpose
   "Flips `mx`"
   [mx]
-  (loop [mx mx                              ; mx is consumed column-by-column
-         mx' []]                            ; mx' is the flipped matrix that is built row-by-row
-    (if (-> mx first empty?)                ; when there are no columns left, we're done
-      mx'
-      (recur (map rest mx)                  ; drop the first colum
-             (conj mx' (mapv first mx)))))) ; append the first column as a row to the flipped matrix
+  (apply map vector mx))
